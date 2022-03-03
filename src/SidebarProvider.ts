e00feab17f7ecc513e9bfd740ac2ffce4b77657f
Joli Lui <joli.lui@taplytics.com>
2022-03-03 15:03:20 -0500

    clean up codes

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -7,11 +7,15 @@ import { getNonce } from "./getNonce";
 
 const PROJECT_KEY = "jolis-tt";
 
+const enum VIEWS {
+  DEFAULT = "default",
+  PROJECT_ID_VIEW = "projectIdView"
+}
 export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView;
   _doc?: vscode.TextDocument;
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+  constructor(private readonly _extensionUri: vscode.Uri) { }
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
     this._view = webviewView;
@@ -30,7 +34,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
         if (res && res.access_token) {
           GlobalStateManager.setState(KEYS.ACCESS_TOKEN, res.access_token);
         }
-        webviewView.webview.html = this.getProjectIdWebview(webviewView.webview);
+        webviewView.webview.html = this._getHtmlForWebview(webviewView.webview, VIEWS.PROJECT_ID_VIEW);
       } else if (data.type === "submitProjectId") {
         let res = await getProject(data.projectId);
         if (res._id) {
@@ -45,60 +49,45 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
     this._view = panel;
   }
 
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-    );
-    const styleVSCodeUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-    );
-
-    const scriptUri = webview.asWebviewUri(
+  private getScriptUri(webview: vscode.Webview, view: string): vscode.Uri {
+    let script = "";
+    if (view === VIEWS.DEFAULT) {
+      script = "sidebar.js"
+    } else if (view === VIEWS.PROJECT_ID_VIEW) {
+      script = "getProjectIdView.js"
+    }
+    return webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "src", "scripts/sidebar.js")
     );
+  }
 
-    const getTokenUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "src", "api/getToken.ts")
-    );
-
-    const nonce = getNonce();
-
-    return `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<!--
-					Use a content security policy to only allow loading images from https or from our extension directory,
-					and only allow scripts that have a specific nonce.
-        -->
-        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource}; script-src 'nonce-${nonce}';">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<link href="${styleResetUri}" rel="stylesheet">
-				<link href="${styleVSCodeUri}" rel="stylesheet">
-        
-        </head>
-        <body>
-        <label>Client Id:</label>
+  private getBodyHtml(view: string): string{
+    let body = "";
+    if (view === VIEWS.DEFAULT) {
+      body = `<label>Client Id:</label>
         <input id="clientId" value="" type="text"></input>
         <label>Client Secret:</label>
         <input id="clientSecret" value="" type="text"></input>
-        <button id="loginBtn">Login</button>
-        </body>
-        <script nonce="${nonce}" src="${scriptUri}"></script>
-        </html>`;
+        <button id="loginBtn">Login</button>`
+    } else if (view === VIEWS.PROJECT_ID_VIEW) {
+      body = `<h1>HI DUMMY</h1>
+        <label>Project Id:</label>
+        <input id="projectId" value="" type="text"></input>
+        <button id="submitBtn">Submit</button>`
+    }
+    return body;
   }
 
-  private getProjectIdWebview(webview: vscode.Webview) {
+  private _getHtmlForWebview(webview: vscode.Webview, view: VIEWS = VIEWS.DEFAULT) {
     const styleResetUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
     );
+
     const styleVSCodeUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
     );
 
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "src", "scripts/getProjectIdView.js")
-    );
+    const scriptUri = this.getScriptUri(webview, view);
 
     const nonce = getNonce();
 
@@ -117,10 +106,7 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
         
         </head>
         <body>
-          <h1>HI DUMMY</h1>
-          <label>Project Id:</label>
-          <input id="projectId" value="" type="text"></input>
-          <button id="submitBtn">Submit</button>
+        ${this.getBodyHtml(view)}
         </body>
         <script nonce="${nonce}" src="${scriptUri}"></script>
         </html>`;
