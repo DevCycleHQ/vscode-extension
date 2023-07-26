d9162f8c91e88aa265f396d7d68266cb724e6fa0
Emma Willis <ehenriks@uwo.ca>
2023-07-26 15:12:28 -0400

    Set up unit and integration tests

---
+++ src/test/suite/extension.integration.test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert'
+import { after } from 'mocha'
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode'
+import { activate } from '../../extension';
+
+suite('Integration Tests', () => {
+  vscode.window.showInformationMessage('Starting integration tests');
+
+  after(() => {
+    vscode.window.showInformationMessage('All tests done!')
+  })
+
+  test('Sample test', () => {
+    vscode.commands.executeCommand('devcycle-featureflags.logout')
+    // TODO 
+  })
+})
