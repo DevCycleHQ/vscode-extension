6fb8bc78484b48577f0275027265edd4e733dcf3
Laura <lauraawarr@gmail.com>
2023-08-24 10:13:55 -0400

    fix: Prevent multiple CLI requests when initializing extension (#110)

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode'
 import { BaseCLIController } from '../cli'
 import { KEYS, StateManager } from '../StateManager'
-import { loadRepoConfig } from './loadRepoConfig'
+import { RepoConfig } from './loadRepoConfig'
 
-export async function autoLoginIfHaveCredentials(folder: vscode.WorkspaceFolder) {
-  const repoConfig = await loadRepoConfig(folder)
+export async function autoLoginIfHaveCredentials(folder: vscode.WorkspaceFolder, repoConfig: RepoConfig) {
   const projectId = repoConfig.project || await StateManager.getFolderState(folder.name, KEYS.PROJECT_ID)
   const organization = repoConfig.org || await StateManager.getFolderState(folder.name, KEYS.ORGANIZATION)
 
