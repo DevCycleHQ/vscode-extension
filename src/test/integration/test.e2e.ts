67ebbfcbf4bedb40e16c0c53bede958a954bab3a
Laura Warr <laura.warr@taplytics.com>
2023-09-01 11:46:47 -0700

    wip

--- src/test/integration/test.e2e.ts
+++ src/test/integration/test.e2e.ts
@@ -1,7 +1,7 @@
-import { browser, $ } from '@wdio/globals'
+import { browser, expect, $ } from '@wdio/globals'
 import { clickSidebarAction, switchToWebview, populateFolderPrompt } from './utils'
 
-describe('Startup View', () => {
+describe('DevCycle Extension E2E', () => {
     it('displays startup view when a folder is not opened', async () => {
         await clickSidebarAction('DevCycle')
         await switchToWebview('#devcycle-startup')
@@ -28,6 +28,23 @@ describe('Startup View', () => {
             const bodyText = await body.getText()
             return bodyText.includes('Login to DevCycle')
         }, { timeoutMsg: 'DevCycle login view did not load' })
+    })
+
+    it('displays expected views after login', async () => {
+        const loginBtn = await $('button=Login')
+        await loginBtn.click()
+
+        const workbench = await browser.getWorkbench()
+        const sidebar = workbench.getSideBar()
+
+        const sections = await sidebar.getContent().getSections()
+
+        expect(sections).toHaveLength(3)
+        const [usagesSection, environmentsSection, resourcesSection] = sections
+
+        expect(await usagesSection.getTitle()).toEqual('Variable Usages')
+        expect(await environmentsSection.getTitle()).toEqual('Environments & Keys')
+        expect(await resourcesSection.getTitle()).toEqual('DevCycle Resources')
     })
 })
 
