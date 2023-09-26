4595ac32cc598c2f7800ba08214a877fd883e386
Jason Salaber <jcsalaber@hotmail.com>
2023-09-26 17:12:15 -0400

    fix: use key if no name defined in sortByName

--- src/views/inspector/utils.ts
+++ src/views/inspector/utils.ts
@@ -2,4 +2,7 @@ import { Feature, Variable } from '../../cli'
 
 export const sortByName = (
   map: Record<string, Variable> | Record<string, Feature>,
-) => Object.values(map).sort((a, b) => a.name.localeCompare(b.name))
+) =>
+  Object.values(map).sort((a, b) =>
+    (a.name || a.key).localeCompare(b.name || b.key),
+  )
