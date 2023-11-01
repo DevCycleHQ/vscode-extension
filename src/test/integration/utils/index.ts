610fc543f3b477723188bfcfe537ea0629c70387
Laura <lauraawarr@gmail.com>
2023-11-01 13:54:37 -0700

    chore: Remove webdriverio (#240)

--- src/test/integration/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './sidebar'
-export * from './switchToWebview'
-export * from './openWorkspaceFolder'
--- src/test/integration/utils/sidebar/clickSidebarAction.ts
+++
@@ -1,7 +0,0 @@
-import { browser } from '@wdio/globals'
-
-export const clickSidebarAction = async (title: string) => {
-  const workbench = await browser.getWorkbench()
-  const view = await workbench.getActivityBar().getViewControl(title)
-  await view?.openView()
-}
